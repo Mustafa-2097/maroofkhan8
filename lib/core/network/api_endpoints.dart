ea0cb63b57b1e3de59233cf0686a2c3834de36cd
Mushfiqur Rahman <mushfiqurrahman.dm@gmail.com>
2026-02-26 14:28:54 +0600

    change password api integration done

--- lib/core/network/api_endpoints.dart
+++ lib/core/network/api_endpoints.dart
@@ -23,18 +23,4 @@ class ApiEndpoints {
   /// Users Profile
   static const String userProfile = '$baseUrl/users/profile';
   static const String updateProfile = '$baseUrl/users/profile';
-
-  /// Live
-  static const String liveScores = '$baseUrl/sports/livescore/Soccer';
-  static String upcomingMatches(String leagueId) =>
-      '$baseUrl/sports/upcoming/$leagueId';
-
-  /// Table
-  static const String leagueTable = '$baseUrl/sports/league-table';
-
-  /// Live TV
-  static const String liveTv = '$baseUrl/contents/live-tv';
-
-  /// News
-  static const String news = '$baseUrl/news?category=sports';
 }
